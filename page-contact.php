7f473666eb970a928e078bbf31d12a012fa0c460
KyleThomsonn <156140131+KyleThomsonn@users.noreply.github.com>
2024-06-29 11:11:13 -0700

    output all contact page content

--- page-contact.php
+++ page-contact.php
@@ -19,14 +19,40 @@ get_header();
 
 		<?php
 		while ( have_posts() ) :
-			the_post();
+			the_post();?>
 
-			get_template_part( 'template-parts/content', 'page' );
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+				<header class="entry-header">
+					<h1>Contact Us</h1>
+				</header>
+				<div class="entry-content">
+
+					<?php if ( function_exists ( 'get_field' ) ) {
+						if(get_field('top_section_info')){
+							?><p><?php the_field('top_section_info')?></p><?php
+						}
+						if ( get_field('contact_email') ) {
+							$email  = get_field( 'contact_email' );
+							$mailto = 'mailto:' . $email;
+							?>
+							<p><a href="<?php echo esc_url( $mailto ); ?> "><?php echo esc_html( $email ); ?></a></p>
+							<?php
+						}
+						if(get_field('contact_phone')){
+							$phone_number = preg_replace('/[^0-9]/', '', get_field('contact_phone'));
+							?><a href="tel:<?php echo $phone_number; ?>"><?php the_field('contact_phone')?></a><?php
+						}
+						if(get_field('address')){
+							?><address><p><?php the_field('address') ?></p></address><?php
+						}
+					}
+
+					the_content();?>
+				</div>
+
+	 		</article>
+			<?php
 
 		endwhile; // End of the loop.
 		?>
@@ -34,5 +60,4 @@ get_header();
 	</main><!-- #main -->
 
 <?php
-get_sidebar();
 get_footer();
