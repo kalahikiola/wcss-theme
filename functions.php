9d99b3c902f54f63165aa8624466e5c0640dc99f
Aaron Bence <kalahikiola8@gmail.com>
2024-07-03 12:05:27 -0700

    Added categories to top of shop pages

--- functions.php
+++ functions.php
@@ -214,6 +214,31 @@ function remove_breadcrumbs() {
 }
 add_filter( 'woocommerce_before_main_content', 'remove_breadcrumbs' );
 
+// Add categories to top of shop pages
+function add_category_links() {
+    if ( ( is_shop() || is_product_category() ) && !is_product_category( 'workshops' ) ) {
+        $product_categories = array("camping", "climbing", "snow");
+
+        echo '<section class="category-links-section">';
+        foreach( $product_categories as $category ) {
+
+            $term = get_term_by( 'slug', sanitize_title( $category ), 'product_cat' );
+            $term_link = get_term_link( $term, 'product_cat' );
+        
+            echo '<article class="category-link">';
+
+            echo '<a href="' . $term_link . '">';
+            echo $term->name;
+            woocommerce_subcategory_thumbnail( $term );
+            echo '</a>';
+
+            echo '</article>';
+        }
+        echo '</section>';
+    }
+}
+add_action( 'woocommerce_shop_loop_header', 'add_category_links', 9);
+
 // Add testimonials to workshop page
 function testimonials_workshop() {
 
