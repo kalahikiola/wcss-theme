bc9bc126d856dfd9753e83cc43cf002ffb9e6ab9
KyleThomsonn <156140131+KyleThomsonn@users.noreply.github.com>
2024-07-07 22:21:47 -0700

    added reviews and additional info single product

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -388,3 +388,5 @@ function remove_woocommerce_product_tabs( $tabs ) {
 add_filter( 'woocommerce_product_tabs', 'remove_woocommerce_product_tabs', 98 );
 
 add_action( 'woocommerce_after_single_product_summary', 'woocommerce_product_description_tab' );
+add_action( 'woocommerce_after_single_product_summary', 'comments_template' );
+add_action( 'woocommerce_after_single_product_summary', 'woocommerce_product_additional_information_tab' );
