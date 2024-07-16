c5d1d9dce33b2877b93a3f42c7ed0c4378f85738
Aaron Bence <kalahikiola8@gmail.com>
2024-07-16 15:23:28 -0700

    Finished removing dashboard widgets

--- functions.php
+++ functions.php
@@ -309,5 +309,7 @@ function remove_dashboard_widgets() {
 	remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );
     remove_meta_box( 'dashboard_primary', 'dashboard', 'side' );
     remove_meta_box( 'dashboard_site_health', 'dashboard', 'normal' );
+    remove_meta_box( 'dashboard_right_now', 'dashboard', 'normal' );
+    remove_meta_box( 'wc_admin_dashboard_setup', 'dashboard', 'normal');
 } 
 add_action( 'wp_dashboard_setup', 'remove_dashboard_widgets' );
